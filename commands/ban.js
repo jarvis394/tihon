6347f7e0db00f10769621030517ce46281022331
jarvis394 <vlad.ekushev@mail.ru>
2019-02-02 21:54:45 +0300

    [cmd] Add description

--- commands/ban.js
+++ commands/ban.js
@@ -44,3 +44,12 @@ exports.run = async (api, update, args) => {
     handleError(update, e);
   }
 }
+
+exports.command = {
+  "name": "ban",
+  "arguments": "(user)|(пользователь)",
+  "description": {
+    "en": "BAN USER (but only if you have special role)",
+    "ru": "ЗАБАНИТЬ! Но только если у тебя есть специальная роль"
+  }
+}
