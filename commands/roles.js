766ad6bc9eb5d2c18ac2af0c108ec908089a5736
jarvis394 <vlad.ekushev@mail.ru>
2019-02-02 15:40:59 +0300

    [cmd] Get everything back (0c896e974e4b14cda7987dc3d0cba17e241484df)

--- commands/roles.js
+++ commands/roles.js
@@ -30,98 +30,114 @@ exports.run = async (api, update, args) => {
     // Error if nothing mathcing
     else return update.send("Не опознал");
 
+    /**
+     * Add role
+     */
+    async function addRole() {
+      let roleName = args[1]; // Role name is the second argument
+      let userId = args[2] && args[2].startsWith("[id") ? // If there is a third argument
+        args[2].slice(1, -1).split("|")[0] : // User ID is the third argument
+        update.senderId; // but if there isn't, userID is sender's ID
+
+      // If no role name
+      if (!roleName) return update.send("⭕️ Не указана роль");
+
+      // Get data
+      let user = await dbDialogGet(userId, update.peerId);
+
+      if (!user) {
+        await dbDialogSet(userId, update.peerId, {
+          "roles": [
+            false
+          ]
+        });
+        user = await dbDialogGet(userId, update.peerId);
+      }
+
+      user.roles = user.roles ? user.roles : [];
+      user.roles.push(roleName);
+      await dbDialogSet(userId, update.peerId, user);
+
+      let name = await api.users.get({
+        user_ids: userId,
+        name_case: "gen"
+      });
+
+      let r = r && r.roles ? r.roles.map(val => {
+        if (val) return "🔸 " + val
+      }).join("\n") : "Пока ничего!";
+
+      return await update.send(`Теперь роли у ${name[0].first_name} ${name[0].last_name}:\n${r}`);
+    }
+
+    /**
+     * Remove role
+     */
+    async function removeRole() {
+      let roleName = args[1]; // Role name is the second argument
+      let userId = args[2] && args[2].startsWith("[id") ? // If there is a third argument
+        args[2].slice(1, -1).split("|")[0] : // User ID is the third argument
+        update.senderId; // but if there isn't, userID is sender's ID
+
+      // If no role name
+      if (!roleName) return update.send("⭕️ Не указана роль");
+
+      // Get data
+      let user = await dbDialogGet(userId, update.peerId);
+
+      if (!user) {
+        await dbDialogSet(userId, update.peerId, {
+          "roles": [
+            false
+          ]
+        });
+        user = await dbDialogGet(userId, update.peerId);
+      }
+
+      let i = user.roles.findIndex(el => el === roleName);
+      if (i !== -1) user.roles.splice(i, 1);
+
+      await dbDialogSet(userId, update.peerId, user);
+
+      let name = await api.users.get({
+        user_ids: userId,
+        name_case: "gen"
+      });
+
+      let r = r && r.roles ? r.roles.map(val => {
+        if (val) return "🔸 " + val
+      }).join("\n") : "Пока ничего!";
+
+      return await update.send(`Теперь роли у ${name[0].first_name} ${name[0].last_name}:\n${r}`);
+    }
+
+    async function showRoles(id) {
+      let r = await dbDialogGet(id, update.peerId);
+
+      if (!r) {
+        await dbDialogSet(id, update.peerId, {
+          "roles": [
+            false
+          ]
+        });
+        r = await dbDialogGet(id, update.peerId);
+      }
+
+      let name = await api.users.get({
+        user_ids: id,
+        name_case: "gen"
+      });
+
+      r = r && r.roles ? r.roles.map(val => {
+        if (val) return "🔸 " + val
+      }).join("\n") : "Пока ничего!";
+
+      return await update.send(`Роли у ${name[0].first_name} ${name[0].last_name}:\n${r}`);
+    }
+
   } catch (e) {
     handleError(update, e)
   }
-}
-
-/**
- * Add role
- */
-async function addRole() {
-  let roleName = args[1]; // Role name is the second argument
-  let userId = args[2] && args[2].startsWith("[id") ? // If there is a third argument
-    args[2].slice(1, -1).split("|")[0] : // User ID is the third argument
-    update.senderId; // but if there isn't, userID is sender's ID
-
-  // If no role name
-  if (!roleName) return update.send("⭕️ Не указана роль");
-
-  // Get data
-  let user = await dbDialogGet(userId, update.peerId);
-
-  if (!user) {
-    await dbDialogSet(userId, update.peerId, {
-      "roles": [
-        "Сельчанин"
-      ]
-    });
-    user = await dbDialogGet(userId, update.peerId);
-  }
-
-  user.roles = user.roles ? user.roles : [];
-  user.roles.push(roleName);
-  await dbDialogSet(userId, update.peerId, user);
-
-  let name = await api.users.get({
-    user_ids: userId,
-    name_case: "gen"
-  });
-
-  let r = user && user.roles ? user.roles.map(val => "🔸 " + val).join("\n") : "Пока ничего!";
-
-  return await update.send(`Теперь роли у ${name[0].first_name} ${name[0].last_name}:\n${r}`);
-}
-
-/**
- * Remove role
- */
-async function removeRole() {
-  let roleName = args[1]; // Role name is the second argument
-  let userId = args[2] && args[2].startsWith("[id") ? // If there is a third argument
-    args[2].slice(1, -1).split("|")[0] : // User ID is the third argument
-    update.senderId; // but if there isn't, userID is sender's ID
-
-  // If no role name
-  if (!roleName) return update.send("⭕️ Не указана роль");
-
-  // Get data
-  let user = await dbDialogGet(userId, update.peerId);
-
-  if (!user) {
-    await dbDialogSet(userId, update.peerId, {
-      "roles": [
-        "Сельчанин"
-      ]
-    });
-    user = await dbDialogGet(userId, update.peerId);
-  }
-
-  let i = user.roles.findIndex(el => el === roleName);
-  if (i !== -1) user.roles.splice(i, 1);
-
-  await dbDialogSet(userId, update.peerId, user);
-
-  let name = await api.users.get({
-    user_ids: userId,
-    name_case: "gen"
-  });
-
-  let r = user && user.roles ? user.roles.map(val => "🔸 " + val).join("\n") : "Пока ничего!";
-
-  return await update.send(`Теперь роли у ${name[0].first_name} ${name[0].last_name}:\n${r}`);
-}
-
-async function showRoles(id) {
-  let r = await dbDialogGet(id, update.peerId);
-  let name = await api.users.get({
-    user_ids: id,
-    name_case: "gen"
-  });
-
-  r = r && r.roles ? r.roles.map(val => "🔸 " + val).join("\n") : "Пока ничего!";
-
-  return await update.send(`Роли у ${name[0].first_name} ${name[0].last_name}:\n${r}`);
 }
 
 exports.command = {
