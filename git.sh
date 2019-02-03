c8cb916c5f883a17f5af44b43dc1139db33a6490
jarvis394 <vlad.ekushev@mail.ru>
2019-02-03 15:52:19 +0300

    [fix] Fix unknown 'origin'

--- git.sh
+++ git.sh
@@ -5,14 +5,14 @@ git fetch origin master
 
 # Delete all files which are being added,
 # so there are no conflicts with untracked files
-for file in `git diff HEAD..origin master --name-status | awk '/^A/ {print $2}'`
+for file in `git diff HEAD..origin/master --name-status | awk '/^A/ {print $2}'`
 do
     echo "Deleting untracked file $file..."
     rm -vf "$file"
 done
 
 # Checkout all files which have been locally modified
-for file in `git diff HEAD..origin master --name-status | awk '/^M/ {print $2}'`
+for file in `git diff HEAD..origin/master --name-status | awk '/^M/ {print $2}'`
 do
     echo "Checking out modified file $file..."
     git checkout $file
