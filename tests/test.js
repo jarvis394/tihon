7f419fedba0ba401d413e22974e7bbfe2d85d1e0
jarvis394 <vlad.ekushev@mail.ru>
2019-03-30 19:10:46 +0300

    [test] Add tests

---
+++ tests/test.js
@@ -0,0 +1,19 @@
+const randomArray = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+const {
+  VK
+} = require('vk-io');
+
+const vk = new VK;
+const {
+  api
+} = vk;
+
+const { TOKEN } = require("../config")
+
+vk.setOptions({
+  token: TOKEN
+});
+
