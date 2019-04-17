8923f429d63b187222e9641c3d80de1101c80955
Glitch (tihon) <none>
2019-04-17 19:08:23 +0000

    💒👨‍❤️‍👨 Checkpoint ./bin/counter.js:1424713/258 ./lib/CoinUser.js:1424713/137

--- bin/counter.js
+++ bin/counter.js
@@ -16,11 +16,14 @@ module.exports = (updates, api, rs) => updates.on("message", async (context, nex
   
   let stData = store.get(context.senderId)
   if (stData) {
-    console.log("there is a data:", stData)
+   /* stData.data.amount++
+    store.set(context.senderId.toString(), stData)
+    */
+    console.log("there is a data for", context.senderId, ":", stData)
   }
   else {
     console.log("no data for", context.senderId)
-    store.
+    store.set(context.senderId.toString(), new CoinUser(context.senderId))
   }
   
   /*if (store.get(context.senderId)) {
