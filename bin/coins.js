595f12de298089f94be016136021e6de2b074070
Glitch (tihon) <none>
2019-04-17 20:40:08 +0000

    🎳🚘 Checkpoint ./bin/coins.js:1424713/411 ./lib/Coins.js:1424713/49

--- bin/coins.js
+++ bin/coins.js
@@ -3,21 +3,28 @@ const { error } = require("../utils")
 const store = require("store")
 const Coins = require("../lib/Coins")
 
+process.on('exit', () => {
+  
+  let st = {}
+  store.each((v, k) => st[k] = v)
+  Coins.flush(st)
+  
+})
+
 module.exports = (updates) => {
   updates.on("message", async (context, next) => {
-  const { senderId } = context
+    const { senderId } = context
   
-  let stData = store.get(senderId)
-  if (stData) {
-    stData.data.amount++
-    store.set(senderId, stData)
-  }
-  else {
-    store.set(senderId, {
-      data: await Coins.data(senderId)
-    })
-  }
+    let stData = store.get(senderId)
+    if (stData) {
+      stData.data.amount++
+      store.set(senderId, stData)
+    } else {
+      store.set(senderId, {
+        data: await Coins.data(senderId)
+      })
+    }
 
-  await next()
-})
+    await next()
+  })
 }
