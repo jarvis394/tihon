f9964e647a34a96910a67adf2d2ef8a294bce058
Glitch (tihon) <none>
2019-04-19 07:50:32 +0000

    🎀🎹 Checkpoint ./commands/shop/shop.js:1424713/809 ./COMMAND_TEMPLATE.js:1424713/408 ./watch.json:1424713/1

--- commands/shop/shop.js
+++ commands/shop/shop.js
@@ -1,4 +1,32 @@
+const { handleError } = require("../../utils")
 
+const {
+  random
+} = require("../../utils")
+
+const aliases = {
+  buy: [ 'buy', 'купить', 'купитт', 'купля', 'ку
+}
+
+exports.run = async (api, update, args) => {
+  try {
+    
+    let option = args[0]
+    
+    if (!option) return sendMenu()
+    if (option in aliases.buy) return sendBuyMenu()
+    if (option in aliases.sell) return sendSellMenu()
+    
+    function sendMenu() {}
+    
+    function sendBuyMenu() {}
+    
+    function sendSellMenu() {}
+    
+  } catch (e) {
+    handleError(update, e)
+  }
+}
 
 exports.command = {
   "name": "shop",
