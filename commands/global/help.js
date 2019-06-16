9977d0b229262a0ddec60beca2c73b944342a86e
Glitch (tihon) <none>
2019-06-16 18:22:29 +0300

    [cmd] Update help command

--- commands/global/help.js
+++ commands/global/help.js
@@ -1,42 +1,22 @@
 exports.run = async (api, update, args, _, __, cmds) => {
   const { handleError } = require('../../utils')
-  
+
   try {
-    let helpText = args[0] === 'en' ? 'Help\n' : 'Помощь\n'
-    let res = ''
-    
-    cmds.forEach(i => {
-      let a
-
-      if (args && args[0] === 'en' && i.arguments)
-        a = i.arguments.split('|')[0] + ' '
-      else if (i.arguments)
-        a = i.arguments.split('|')[1] + ' '
-      else
-        a = ''
-
-      let desc = args[0] ? i.description.en : i.description.ru
-
-      res += `▫️ /${i.name} ${a}- ${desc}\n`
-    })
-
-    await update.send('📃 ' + helpText + res)
+    return update.send(
+      '🔍 Смотри команды на сайте: https://tihon-web.glitch.me'
+    )
   } catch (e) {
     handleError(update, e)
   }
 }
 
 exports.command = {
-  'name': 'help',
-  'arguments': '(lang)|(язык)',
-  'description': {
-    'en': 'Helps you find a description of the command you need',
-    'ru': 'Помогает найти нужную тебе команду'
+  name: 'help',
+  arguments: false,
+  description: {
+    en: 'Helps you find a description of the command you need',
+    ru: 'Помогает найти нужную тебе команду'
   },
-  'alias': [
-    'помощь',
-    'справка',
-    'начать'
-  ],
-  'group': 'global'
+  alias: ['помощь', 'справка', 'начать'],
+  group: 'global'
 }
