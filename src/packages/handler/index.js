3ef55abaf60f96be1e958b06a4ee9d607ee1f37f
jarvis394 <vlad.ekushev@mail.ru>
2019-10-19 16:32:08 +0300

    fix: fix update context

--- src/packages/handler/index.js
+++ src/packages/handler/index.js
@@ -3,22 +3,22 @@ const handleError = require('../../utils/handleError')
 const { vk } = require('../../globals')
 const { MessageContext } = require('vk-io')
 
-cluster.worker.on('message', async ({ isCommand, update, state }) => {
-  if (!isCommand) return 
+cluster.worker.on('message', async ({ isCommand, update: payload, state }) => {
+  if (!isCommand) return
 
-  const u = new MessageContext(vk, update, {})
-  
+  const update = new MessageContext(vk, payload, {})
+
   // Destructure state
   const { command: cmd, arguments: args } = state
 
   try {
     const command = require(`../../commands/${cmd.group}/${cmd.name}`)
-    const response = await command.run({ u, args })
-    
-    await u.reply(response)
+    const response = await command.run({ update, args })
+
+    await update.reply(response)
     return cluster.worker.send({ busy: false })
   } catch (e) {
     if (e.code === 'MODULE_NOT_FOUND') return
-    else return handleError(u, e)
+    else return handleError(update, e)
   }
 })
